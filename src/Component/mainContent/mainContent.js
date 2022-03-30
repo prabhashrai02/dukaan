fa3beb2ea22ea963d8ff50f23787fd41f5b51044
prabhashrai02 <prabhashsahaj@gmail.com>
2022-03-30 19:12:16 +0530

    pagination added

--- src/Component/mainContent/mainContent.js
+++ src/Component/mainContent/mainContent.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from '../button/button';
+import Pagination from '../pagination/pagination';
 import './mainContent.css';
 
 const Maincontent = () => {
@@ -24,166 +25,17 @@ const Maincontent = () => {
         "coziness building for tomorrow",
         "coziness building for tomorrow",
         "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "There is no Sore it will Not Heal, No cool it will not Subdue.",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
-        "coziness building for tomorrow",
+        "socond",
     ]
 
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(15);
+    const [postsPerPage] = useState(10);
 
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = slogan.slice(indexOfFirstPost, indexOfLastPost);
-    
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
     return (
         <div className="maincontent">
             <div className="maincontent_body">
@@ -191,23 +43,29 @@ const Maincontent = () => {
                     Free slogan maker
                 </div>
                 <div className="maincontent_text">
-                Simply enter a term that describes your business, and get up to 1,000 relevant slogans for free.
+                    Simply enter a term that describes your business, and get up to 1,000 relevant slogans for free.
                 </div>
                 <div className="maincontent_Search_heading">
                     Word for your slogan
                 </div>
-                <input type="text" className="maincontent_searchbox"/>
-                <br/>
+                <input type="text" className="maincontent_searchbox" />
+                <br />
                 <Button className="maincontent_generate_button">Generate slogans</Button>
                 <div className="maincontent_line"></div>
-                <div className= "maincontent_data_box">
+                <div className="maincontent_data_box">
                     {
-                        currentPosts.map((data, index) => 
-                        <li key={index} className="maincontent_data">
-                            {data}
-                        </li>
+                        currentPosts.map((data, index) =>
+                            <li key={index} className="maincontent_data">
+                                {data}
+                            </li>
+
                     )}
                 </div>
+                <Pagination
+                    postsPerPage={postsPerPage}
+                    totalPosts={slogan.length}
+                    paginate={paginate}
+                />
             </div>
         </div>
     )
